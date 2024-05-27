09cdf14c8bf152ef892eb49c20ad63fe0e932c2f
Bruno <brunombonifacio@gmail.com>
2024-05-27 17:48:22 -0300

    Added fields to sign up page

--- pages/signup.php
+++ pages/signup.php
@@ -12,11 +12,37 @@
         <h1>Criar conta</h1>
         
         <form action="" method="post" class="">
-            <div class="form-floating">
-                <input type="text" name="nome" id="nome" class="form-control">
-                <label for="nome">Nome completo</label>
+
+            <div class="row align-items-start m-2">
+                <div class="col">
+                    <div class="form-floating">
+                        <input type="text" name="nome" id="nome" class="form-control">
+                        <label for="nome">Nome Completo</label>
+                    </div>
+                </div>
             </div>
-            <div class="row align-items-start">
+            <div class="row align-items-start m-2">
+                <div class="col">
+                    <div class="form-floating">
+                        <input type="email" name="email" id="email" class="form-control">
+                        <label for="nome">Endereço de E-mail</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-floating">
+                        <input type="text" name="telefone" id="telefone" class="form-control">
+                        <label for="nome">Telefone</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-floating">
+                        <input type="text" name="whastapp" id="whatsapp" class="form-control">
+                        <label for="nome">Número WhatsApp</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-start m-2">
                 <div class="col">
                     <div class="form-floating">
                         <input type="text" name="cpf" id="cpf" class="form-control">
@@ -25,35 +51,46 @@
                 </div>
                 <div class="col">
                     <div class="form-floating">
-                        <input type="date" name="nome" id="nome" class="form-control">
+                        <input type="date" name="data_nasc" id="dataNasc" class="form-control">
                         <label for="nome">Data de Nascimento</label>
                     </div>
                 </div>
             </div>
-            <div class="form-floating">
-                <input type="email" name="email" id="email" class="form-control">
-                <label for="nome">Endereço de E-mail</label>
+            
+            <div class="row align-items-start m-2">
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example" name="estado" id="estado">
+                        <option selected>Estado</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example" name="cidade" id="cidade" disabled>
+                        <option selected>Cidade</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
             </div>
-            <div class="form-floating">
-                <input type="text" name="nome" id="nome" class="form-control">
-                <label for="nome">Nome completo</label>
+
+            <div class="row align-items-start m-2">
+                <div class="col">
+                    <div class="form-floating">
+                        <input type="text" name="nome" id="nome" class="form-control">
+                        <label for="nome">Senha</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-floating">
+                        <input type="text" name="nome" id="nome" class="form-control">
+                        <label for="nome">Confirme sua Senha</label>
+                    </div>
+                </div>
             </div>
-            <div class="form-floating">
-                <input type="text" name="nome" id="nome" class="form-control">
-                <label for="nome">Nome completo</label>
-            </div>
-            <div class="form-floating">
-                <input type="text" name="nome" id="nome" class="form-control">
-                <label for="nome">Nome completo</label>
-            </div>
-            <div class="form-floating">
-                <input type="text" name="nome" id="nome" class="form-control">
-                <label for="nome">Nome completo</label>
-            </div>
-            <div class="form-floating">
-                <input type="text" name="nome" id="nome" class="form-control">
-                <label for="nome">Nome completo</label>
-            </div>
+
         </form>
     </div>
 </body>
