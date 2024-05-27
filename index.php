f45561674ae8c8f89fcef0f29a5e95120469beab
Bruno <brunombonifacio@gmail.com>
2024-05-27 15:33:53 -0300

    Added index.php and pages folder

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ouvidoria</title>
+</head>
+<body>
+    Bem vindo ao site da Ouvidoria publica
+    <a href="paginas/cadastro.php">Criar uma conta</a>
+    Já tem uma conta? <a href="paginas/login.php">Fazer login</a>
+</body>
+</html>
