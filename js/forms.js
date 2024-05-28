742a38429cecae8c82c468041cc6eb67e8eb65e5
Bruno <brunombonifacio@gmail.com>
2024-05-28 16:58:09 -0300

    Fixed Cidade and Estado selectors
    Dynamic selectors Cidade and Estado are now working correctly in the Sign-Up page

--- js/forms.js
+++ js/forms.js
@@ -1,18 +1,65 @@
-function getStates() {
-    $.post('../pphp/address.php', {function: 'getStates'})
+async function getStates() {
+    var states = []
+    await $.post('../php/address.php', {function: 'getStates'}, (response) => {
+        responseArr = response.split('//\\').filter(state => state.trim())
+
+        responseArr.forEach(state => states.push(JSON.parse(state)))
+    })
+
+    return states
 }
 
-function getCities(stateId) {
-    $.post('../php/address.php', {function: 'getCities', state: stateId}, (response) => {
+async function getCities(stateId) {
+    var cities = []
+    await $.post('../php/address.php', {function: 'getCities', state: stateId}, (response) => {
+        responseArr = response.split('//\\').filter(state => state.trim())
 
+        responseArr.forEach(city => cities.push(JSON.parse(city)))
+    })
+
+    return cities
+}
+
+if ($('estado')) {
+    getStates().then(states => {
+        states.forEach(state => {
+
+            const option = document.createElement('option')
+            option.value = state.id_estado
+            option.innerText = state.nome_estado
+            
+            document.getElementsByName('estado').forEach(select => select.append(option))
+        })
     })
 }
 
+if ($('cidade')) {
+    document.getElementsByName('estado').forEach(element => 
+        element.addEventListener('change', () => {
 
-if ($('estado')) {
-    let option = document.createElement('option')
-    option.value = '1'
-    option.innerText = 'pinto'
+            document.getElementsByName('cidade').forEach(select => select.innerHTML = `<option selected>Cidade</option>`)
 
-    document.querySelector('estado').append(option)
+            var state = $(element).val()
+            
+            if (state == 'Estado') {
+                document.getElementsByName('cidade').forEach(select => select.setAttribute('disabled', 'true'))
+                return
+            }
+
+            document.getElementsByName('cidade').forEach(select => select.removeAttribute('disabled'))
+            
+            getCities(state).then(cities => {
+                cities.forEach(city => {
+
+                    const option = document.createElement('option')
+                    option.value = city.id_cidade
+                    option.innerText = city.nome_cidade
+
+                    document.getElementsByName('cidade').forEach(select => select.append(option))
+                })
+            }
+            )
+
+        })
+    )
 }
--- php/address.php
+++ php/address.php
@@ -5,18 +5,52 @@ $functions = [
     'getStates' => function() {
         include "../db-connection/connection.php";
 
-        $query = $connection->prepare('SELECT * FROM estado SORT BY nome_estado');
+        $query = $connection->prepare("SELECT id_estado, nome_estado FROM estado ORDER BY nome_estado");
 
-        echo $query;
+        if (!$query->execute()) {
+            echo "[]";
+            return;
+        }
+
+        if (!$query->rowCount()) {
+            echo "[]";
+            return;
+        }
+
+        $states = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($states as $state) {
+            echo json_encode($state) . ' //\\ ';
+        }
 
     },
 
     'getCities' => function() {
-        
+        include "../db-connection/connection.php";
+
+        $stateId = $_POST['state'];
+
+        $query = $connection->prepare("SELECT * FROM cidade WHERE cod_estado = '$stateId' ORDER BY nome_cidade");
+
+        if (!$query->execute()) {
+            echo "[]";
+            return;
+        }
+
+        if (!$query->rowCount()) {
+            echo "[]";
+            return;
+        }
+
+        $states = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($states as $state) {
+            echo json_encode($state) . ' //\\ ';
+        }
     }
 ];
 
-$function = $_POST['function'];
+$function = $_POST['function'] ?? null;
 if (key_exists($function, $functions)) {
     $functions[$function]();
 }
