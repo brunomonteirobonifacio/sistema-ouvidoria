58b24c5c0531dab228ec77320b2f8ee497ef0647
Bruno <brunombonifacio@gmail.com>
2024-05-28 14:41:42 -0300

    Update forms.js

--- js/forms.js
+++ js/forms.js
@@ -1,3 +1,18 @@
+function getStates() {
+    $.post('../pphp/address.php', {function: 'getStates'})
+}
+
 function getCities(stateId) {
-    $.post('../php/address.php', {function: 'getCities'})
+    $.post('../php/address.php', {function: 'getCities', state: stateId}, (response) => {
+
+    })
+}
+
+
+if ($('estado')) {
+    let option = document.createElement('option')
+    option.value = '1'
+    option.innerText = 'pinto'
+
+    document.querySelector('estado').append(option)
 }
