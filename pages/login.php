8a70b6aeb7efa95aa34c211a331b9b0ba8f4c983
Bruno <brunombonifacio@gmail.com>
2024-05-28 16:59:08 -0300

    Update login.php
    Started working on Login page

--- pages/login.php
+++ pages/login.php
@@ -8,10 +8,42 @@
     <title>Entrar</title>
 </head>
 <body>
-    
+<div class="container px-auto">
+        <h1 class="m-2 p-2">Entrar</h1>
+        
+        <form action="" method="post" class="">
+            <div class="row align-items-start m-2">
+                <div class="col">
+                    <div class="form-floating">
+                        <input type="email" name="email" id="email" class="form-control">
+                        <label for="email">Endereço de E-mail</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-start m-2">
+                <div class="col">
+                    <div class="form-floating">
+                        <input type="password" name="senha" id="senha" class="form-control">
+                        <label for="nome">Senha</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center m-2">
+                <div class="col">
+                    <button type="button" class="btn btn-primary" name="login_btn">Entrar</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 <script
   src="https://code.jquery.com/jquery-3.7.1.min.js"
   integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
   crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" integrity="sha512-0XDfGxFliYJPFrideYOoxdgNIvrwGTLnmK20xZbCAvPfLGQMzHUsaqZK8ZoH+luXGRxTrS46+Aq400nCnAT0/w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="../js/masks.js"></script>
+  <script src="../js/forms.js"></script>
 </html>
