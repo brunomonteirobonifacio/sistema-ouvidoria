cafe7b0a677030e625bd47c086eadb10a57f42b0
Bruno <brunombonifacio@gmail.com>
2024-05-28 23:18:09 -0300

    Update user.php
    Created functions createUser(), loginUser() and login()

--- php/user.php
+++ php/user.php
@@ -1,9 +1,61 @@
 <?php
 // This file is dedicated to user-related functions in database, such as sign-up and log-in
+function login($email) {
+    include "../db-connection/connection.php";
+    session_start();
+
+    $query = $connection->prepare("SELECT id_usuario FROM usuario WHERE email_usuario = :email");
+    $query->bindvalue('email', $email);
+    
+    $query->execute();
+
+    $userId = $query->fetchAll(PDO::FETCH_ASSOC)[0]['id_usuario'];
+
+    $_SESSION['userId'] = $userId;
+    exit();
+}
 
 $functions = [
     'createUser' => function() {
+        include "../db-connection/connection.php";
+        include "../config.php";
+
+        $username = $_POST['nome'];
+        $email = $_POST['email'];
+        $phone = $_POST['telefone'];
+        $whatsapp = $_POST['whatsapp'];
+        $cpf = $_POST['cpf'];
+        $birthDate = $_POST['data_nasc'];
+        $stateId = $_POST['estado'];
+        $cityId = $_POST['cidade'];
+        $pass = $_POST['senha'] . $_ENV['pepper'];
+
+        $query = $connection->prepare("INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario, whatsapp_usuario, cpf_usuario, data_nasc, cod_estado, cod_cidade, senha_usuario) VALUES
+        (:username, :email, :phone, :whatsapp, :cpf, :birthDate, :stateId, :cityId, SHA512(:pass))");
+
+        $query->bindValue('username', $username);
+        $query->bindValue('email', $email);
+        $query->bindValue('phone', $phone);
+        $query->bindValue('whatsapp', $whatsapp);
+        $query->bindValue('cpf', $cpf);
+        $query->bindValue('birthDate', $birthDate);
+        $query->bindValue('stateId', $stateId);
+        $query->bindValue('cityId', $cityId);
+        $query->bindValue('pass', $pass);
+
+        if (!$query->execute()) {
+            echo "Status 500";
+            exit();
+        }
         
+        login($email);
+
+        echo "Status 201";
+        exit();
+    },
+
+    'loginUser' => function() {
+        login($_POST['email']);
     }
 ];
 
