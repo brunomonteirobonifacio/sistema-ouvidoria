8d2c8362923d849bd02139f159edfee7674ef576
Bruno <brunombonifacio@gmail.com>
2024-05-29 13:50:00 -0300

    Exposed config.php

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+$_ENV['pepper'] = '%\\wh5oX6';
+$_ENV['db_config'] = ['localhost', 'root', '', 'sistema_ouvidoria'];
--- db-connection/connection.php
+++ db-connection/connection.php
@@ -1,10 +1,5 @@
 <?php
 require "../config.php";
-// this file is absent on GitHub
-// Its structure can be seen below:
-
-// $_ENV['pepper'] = *password pepper*;
-// $_ENV['db_config'] = [*host*, *user*, *password*, *database*];
 
 [$host, $user, $pass, $database] = $_ENV['db_config'];
 
