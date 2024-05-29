0383abaaa8a321c25715b4538d5a2490f618e517
Bruno <brunombonifacio@gmail.com>
2024-05-29 17:09:09 -0300

    Downloaded JQuery, JQuery Mask and Bootstrap

--- pages/signup.php
+++ pages/signup.php
@@ -131,9 +131,11 @@
   src="https://code.jquery.com/jquery-3.7.1.min.js"
   integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
   crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" integrity="sha512-0XDfGxFliYJPFrideYOoxdgNIvrwGTLnmK20xZbCAvPfLGQMzHUsaqZK8ZoH+luXGRxTrS46+Aq400nCnAT0/w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="../js/masks.js"></script>
-  <script src="../js/forms.js"></script>
-  <script src="../js/forms_validation.js"></script>
+  <script src="../jquery/jquery.js"></script>
+  <script src="../jquery/jquery-mask/dist/jquery.mask.js"></script>
+  <script src="../jquery/jquery-mask/dist/jquery.mask.min.js"></script>
+  
+  <script src="../js-scripts/masks.js"></script>
+  <script src="../js-scripts/forms.js"></script>
+  <script src="../js-scripts/forms_validation.js"></script>
 </html>
