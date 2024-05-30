f4b0c754279bbd36311f446da4bcf4840b2d9b1b
Bruno <brunombonifacio@gmail.com>
2024-05-30 16:13:34 -0300

    Update connection.php

--- db-connection/connection.php
+++ db-connection/connection.php
@@ -4,3 +4,9 @@ require "../config.php";
 [$host, $user, $pass, $database] = $_ENV['db_config'];
 
 $connection = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
+
+// assures that real prepared statements are being used
+$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+// throws exceptions when mysql errors happen
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
