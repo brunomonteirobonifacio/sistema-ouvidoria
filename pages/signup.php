4795cd3fbdafabe1679961464701f208df9507c0
Bruno <brunombonifacio@gmail.com>
2024-05-30 22:27:52 -0300

    Removed JQuery and Bootstrap CDNs

--- pages/signup.php
+++ pages/signup.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    
     <title>Crie uma conta</title>
 </head>
 <body>
@@ -17,7 +17,7 @@
             </div>
         </div>
         
-        <form method="post" class="needs-validation" novalidate>
+        <form method="post" id="signup" class="needs-validation" novalidate>
             
             <div class="row align-items-start m-2">
                 <div class="col">
@@ -139,16 +139,14 @@
         </form>
     </div>
 </body>
-<script
-  src="https://code.jquery.com/jquery-3.7.1.min.js"
-  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
-  crossorigin="anonymous"></script>
-  <script src="../jquery/jquery.js"></script>
-  <script src="../jquery/jquery-mask/dist/jquery.mask.js"></script>
-  <script src="../jquery/jquery-mask/dist/jquery.mask.min.js"></script>
-  
-  <script src="../js-scripts/masks.js"></script>
-  <script src="../js-scripts/forms.js"></script>
-  <script src="../js-scripts/forms_validation.js"></script>
-  <script src="../js-scripts/user.js"></script>
+<script src="../jquery/jquery.js"></script>
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script src="../jquery/jquery-mask/dist/jquery.mask.js"></script>
+<script src="../jquery/jquery-mask/dist/jquery.mask.min.js"></script>
+
+<script src="../js-scripts/masks.js"></script>
+<script src="../js-scripts/forms.js"></script>
+<script src="../js-scripts/forms_validation.js"></script>
+<script src="../js-scripts/user.js"></script>
+
 </html>
