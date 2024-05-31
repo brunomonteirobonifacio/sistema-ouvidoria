aa3340a9e3aec5d52364f8ded595e25caa6c7bb9
Bruno <brunombonifacio@gmail.com>
2024-05-31 15:21:01 -0300

    Removed unused condition, added comments

--- js-scripts/signup_form.js
+++ js-scripts/signup_form.js
@@ -1,54 +1,55 @@
-// this function won't do anything by standard, only if the user registration was successful will it redirect to homepage
+// =========================================================================================
+// this file contains functions used exclusively in signup
+// =========================================================================================
+
+// this will be the function called when success or failure modal closes, and it will determine what will happen when modal is hidden
+// it won't do anything by standard, only if the user registration was successful will it redirect to homepage
 var closeModalAction = () => {}
 
 // check form validity, then sign up
-if ($('button#signup_btn')) {
-    // this will be the function called when success or failure modal closes, and it will determine what will happen when modal is hidden
+$('button#signup_btn').on('click', async () => {
+    var form = document.querySelector('form.needs-validation')
 
-    $('button#signup_btn').on('click', async () => {
-        var form = document.querySelector('form.needs-validation')
-
-        
-        // boolean function, checks each field of form and returns true if it's all valid
-        const validForm = await checkFormValidity(form)
-        
-        if (!validForm) return
-        
-        // using FormData.entries() to create an object structured as {*input[i]_name*: *input[i]_value*}
-        const formData = new FormData(form)
-        const dataObj = Object.fromEntries(formData.entries())
-        
-        // sign up user with given data
-        const userCreated = await createUser(dataObj)
-        
-        // activate error message modal if there was an error in user registration
-        if (!userCreated) {
-            $('#signupModalLabel').text('Erro ao realizar o cadastro')
-            $('.modal-body').text('Ocorreu um erro na realização de seu cadastro. Por favor, tente novamente mais tarde.')
-
-            $('#confirmModalBtn').hide()
-            $('#closeModalBtn').show()
-
-            $('#signupModal').modal('show')
-            
-            return
-        } 
-        
-        // will set closeModalAction to direct to homepage if registration was successful
-        closeModalAction = () => {
-            window.location.href = '../'
-        }
-
-        // else activate success message modal
-        $('#signupModalLabel').text('Sucesso!')
-        $('.modal-body').text('Seu cadastro foi realizado com sucesso!')
-
-        $('#closeModalBtn').hide()
-        $('#confirmModalBtn').show()
-
+    
+    // boolean function, checks each field of form and returns true if it's all valid
+    const validForm = await checkFormValidity(form)
+    
+    if (!validForm) return
+    
+    // using FormData.entries() to create an object structured as {*input[i]_name*: *input[i]_value*}
+    const formData = new FormData(form)
+    const dataObj = Object.fromEntries(formData.entries())
+    
+    // sign up user with given data
+    const userCreated = await createUser(dataObj)
+    
+    // activate error message modal if there was an error in user registration
+    if (!userCreated) {
+        $('#signupModalLabel').text('Erro ao realizar o cadastro')
+        $('.modal-body').text('Ocorreu um erro na realização de seu cadastro. Por favor, tente novamente mais tarde.')
+    
+        $('#confirmModalBtn').hide()
+        $('#closeModalBtn').show()
+    
         $('#signupModal').modal('show')
-    })
-}
+        
+        return
+    } 
+    
+    // will set closeModalAction to direct to homepage if registration was successful
+    closeModalAction = () => {
+        window.location.href = '../'
+    }
+
+    // else activate success message modal
+    $('#signupModalLabel').text('Sucesso!')
+    $('.modal-body').text('Seu cadastro foi realizado com sucesso!')
+
+    $('#closeModalBtn').hide()
+    $('#confirmModalBtn').show()
+
+    $('#signupModal').modal('show')
+})
 
 if ($('#signupModal')) {
     $('#signupModal').on('hide.bs.modal', () => {
