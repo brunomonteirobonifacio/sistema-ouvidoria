1f4d204333d0ea85b9e931528a0aec0013dae02e
Bruno <brunombonifacio@gmail.com>
2024-05-31 14:27:54 -0300

    Moved Signup exclusive functions
    Moved functions only used in Signup to 'signup_forms.js' file.

--- js-scripts/signup_form.js
+++ js-scripts/signup_form.js
@@ -1,29 +1,58 @@
+// this function won't do anything by standard, only if the user registration was successful will it redirect to homepage
+var closeModalAction = () => {}
+
 // check form validity, then sign up
 if ($('button#signup_btn')) {
-    $('button#signup_btn').on('click', async () => {
+    // this will be the function called when success or failure modal closes, and it will determine what will happen when modal is hidden
 
+    $('button#signup_btn').on('click', async () => {
         var form = document.querySelector('form.needs-validation')
+
         
         // boolean function, checks each field of form and returns true if it's all valid
         const validForm = await checkFormValidity(form)
         
         if (!validForm) return
-
+        
         // using FormData.entries() to create an object structured as {*input[i]_name*: *input[i]_value*}
         const formData = new FormData(form)
         const dataObj = Object.fromEntries(formData.entries())
         
         // sign up user with given data
         const userCreated = await createUser(dataObj)
-
-        // User creation working
-        // TODO: activate a modal to show when user registration succeeds (or fails)
+        
+        // activate error message modal if there was an error in user registration
         if (!userCreated) {
-            // activate error message modal
+            $('#signupModalLabel').text('Erro ao realizar o cadastro')
+            $('.modal-body').text('Ocorreu um erro na realização de seu cadastro. Por favor, tente novamente mais tarde.')
+
+            $('#confirmModalBtn').hide()
+            $('#closeModalBtn').show()
+
+            $('#signupModal').modal('show')
+            
             return
         } 
         
+        // will set closeModalAction to direct to homepage if registration was successful
+        closeModalAction = () => {
+            window.location.href = '../'
+        }
+
         // else activate success message modal
+        $('#signupModalLabel').text('Sucesso!')
+        $('.modal-body').text('Seu cadastro foi realizado com sucesso!')
+
+        $('#closeModalBtn').hide()
+        $('#confirmModalBtn').show()
+
+        $('#signupModal').modal('show')
+    })
+}
+
+if ($('#signupModal')) {
+    $('#signupModal').on('hide.bs.modal', () => {
+        closeModalAction()
     })
 }
 
