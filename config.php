be80ae1dcda77d623a441fdf6af95b139d6abeba
Bruno <brunombonifacio@gmail.com>
2024-05-31 15:21:21 -0300

    Added explanatory comments

--- config.php
+++ config.php
@@ -1,3 +1,7 @@
 <?php
+// =========================================================================================
+// this file contains envinronmental variables and database settings used within the system
+// =========================================================================================
+
 $_ENV['pepper'] = '%\\wh5oX6';
 $_ENV['db_config'] = ['localhost', 'root', '', 'sistema_ouvidoria'];
