03ce4da5ecbfb81bec616d250e32c7048b3a512e
Bruno <brunombonifacio@gmail.com>
2024-05-31 18:20:04 -0300

    Started working on Login form

--- js-scripts/login_form.js
+++ js-scripts/login_form.js
@@ -1,4 +1,22 @@
 // =========================================================================================
 // this file contains functions used for form utilities within the system
 // =========================================================================================
+var form = document.querySelector('form.needs-validation')
 
+$('button#login_btn').on('click', async () => {
+    // only logs in if all fields are checked
+    if (!checkEmptyFields(form)) return
+
+
+})
+
+
+// validates invalid and valid fields again every time the user changes input values
+form.querySelectorAll('input[required]').forEach(field => {
+    field.addEventListener('blur', () => {
+        field.value = field.value.trim()
+
+        validateField[field.name](field, form)
+        debugger
+    })
+})
