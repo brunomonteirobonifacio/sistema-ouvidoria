4b7488d9efc7fc1cf79d54df38cdd9a4dc270168
Bruno <brunombonifacio@gmail.com>
2024-06-01 20:39:28 -0300

    Started working on manifestation form

--- pages/create_manifestation.php
+++ pages/create_manifestation.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <title>Criar Ouvidoria</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>Abrir Ouvidoria</title>
 </head>
 <body>
     <!-- Modal -->
@@ -29,7 +29,7 @@
     <div class="container text-center p-3 mx-auto my-3 border border-dark-subtle rounded w-50">
         <div class="row align-items-center m-2 d-inline-flex">
             <div class="col">
-                <h1 class="m-2 pd-2 display-2 border-bottom border-dark-subtle">Criar conta</h1>
+                <h1 class="m-2 pd-2 display-2 border-bottom border-dark-subtle">Abrir ouvidoria</h1>
             </div>
         </div>
         
@@ -38,10 +38,10 @@
             <div class="row align-items-start m-2">
                 <div class="col">
                     <div class="form-floating">
-                        <input type="text" name="name" id="name" class="form-control" autocomplete="off" required>
-                        <label for="name">Nome Completo *</label>
+                        <textarea name="description" id="description" class="form-control" autocomplete="off" rows="3" required></textarea>
+                        <label for="description">Descreva o motivo de sua ouvidoria *</label>
                         <div class="invalid-feedback" style="text-align: start">
-                            Por favor, digite seu nome completo.
+                            Este campo é obrigatório.
                         </div>
                     </div>
                 </div>
@@ -49,100 +49,28 @@
             
             <div class="row align-items-start m-2">
                 <div class="col">
-                    <div class="form-floating">
-                        <input type="email" name="email" id="email" class="form-control" autocomplete="off" required>
-                        <label for="email">Endereço de E-mail *</label>
-                        <div class="invalid-feedback" id="invalid-email" style="text-align: start">
-                            Digite um endereço de E-mail válido.
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row align-items-start m-2">
-                <div class="col">
-                    <div class="form-floating">
-                        <input type="text" name="phone" id="phone" class="form-control phone" autocomplete="off" required>
-                        <label for="phone">Telefone *</label>
-                        <div class="invalid-feedback" id="invalid-phone" style="text-align: start">
-                            Digite um número de telefone válido.
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="form-floating">
-                        <input type="text" name="whatsapp" id="whatsapp" class="form-control phone" autocomplete="off" required>
-                        <label for="whatsapp">Número WhatsApp *</label>
-                        <div class="invalid-feedback" id="invalid-whatsapp" style="text-align: start">
-                            Digite um número WhatsApp válido.
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row align-items-start m-2">
-                <div class="col">
-                    <div class="form-floating">
-                        <input type="text" name="cpf" id="cpf" class="form-control cpf" autocomplete="off" required>
-                        <label for="cpf">CPF *</label>
-                        <div class="invalid-feedback" id="invalid-cpf" style="text-align: start">
-                            Digite um CPF válido.
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="form-floating">
-                        <input type="date" name="birthdate" id="birthdate" class="form-control" autocomplete="off" required>
-                        <label for="birthdate">Data de Nascimento *</label>
-                        <div class="invalid-feedback" id="invalid-date" style="text-align: start">
-                            É necessário ter mais de 18 anos para realizar o cadastro.
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="row align-items-start m-2">
-                <div class="col">
-                    <select class="form-select"  name="state" id="state" required>
-                        <option selected>Estado *</option>
+                    <select class="form-select"  name="service-type" id="service-type" required>
+                        <option selected>Tipo de serviço afetado *</option>
                     </select>
                     <div class="invalid-feedback" style="text-align: start">
-                            Por favor, escolha seu estado.
+                        Este campo é obrigatório.
                     </div>
                 </div>
                 <div class="col">
-                    <select class="form-select" name="city" id="city" disabled required>
-                        <option selected>Cidade *</option>
+                    <select class="form-select" name="manifestation-type" id="manifestation-type" required>
+                        <option selected>Tipo de manifestação *</option>
                     </select>
                     <div class="invalid-feedback" style="text-align: start">
-                            Por favor, escolha sua cidade.
+                        Este campo é obrigatório.
                     </div>
                 </div>
             </div>
             
             <div class="row align-items-start m-2">
                 <div class="col">
-                    <div class="form-floating">
-                        <input type="password" name="password" id="password" class="form-control" required>
-                        <label for="password">Senha *</label>
-                        <div class="invalid-feedback" id="invalid-password" style="text-align: start">
-                            Digite uma senha válida.
-                        </div>
-                        <p style="text-align: start" class="password_requirements lead fs-6">
-                            Sua senha deve conter:
-                            <ul style="text-align: start" class="lead fs-6">
-                                <li>Pelo menos 8 caracteres</li>
-                                <li>Letras maiúsculas e minúsculas</li>
-                                <li>Pelo menos um caracter especial</li>
-                                <li>Pelo menos um dígito</li>
-                            </ul>
-                        </p>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="form-floating">
-                        <input type="password" name="confirm_password" id="confirm_password" class="form-control" required>
-                        <label for="confirm_password">Confirme sua Senha *</label>
+                    <div class="input-group mb-3">
+                      <label class="input-group-text" for="inputGroupFile01">Anexos</label>
+                      <input type="file" class="form-control" id="manifestation-attachments" multiple="multiple"    >
                     </div>
                 </div>
             </div>
@@ -155,10 +83,10 @@
         </form>
     </div>
 </body>
-<script src="jquery/jquery.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="jquery/jquery-mask/dist/jquery.mask.js"></script>
-<script src="jquery/jquery-mask/dist/jquery.mask.min.js"></script>
+<script src="../jquery/jquery.js"></script>
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script src="../jquery/jquery-mask/dist/jquery.mask.js"></script>
+<script src="../jquery/jquery-mask/dist/jquery.mask.min.js"></script>
 
-<script src="js-scripts/user.js"></script>
+<script src="../js-scripts/user.js"></script>
 </html>
