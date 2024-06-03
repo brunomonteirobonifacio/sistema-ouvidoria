2adb3c13342a467bd1e972dd7563b43a60e26bfe
Bruno <brunombonifacio@gmail.com>
2024-06-03 17:56:44 -0300

    Fixed getServiceTypes and getManifestationTypes

--- js-scripts/manifestation.js
+++ js-scripts/manifestation.js
@@ -1,13 +1,26 @@
 async function getServiceTypes() {
+    var serviceTypes = [];
+
+    // gets all service types from table `servico_afetado` and adds them all as Objects to serviceTypes array
+    await $.post(`${getPhpPath()}/manifestation.php`, { function: 'getServiceTypes' }, (response) => {
+        const responseArr = response.split('//\\').filter(serviceType => serviceType.trim());
+        
+        responseArr.forEach(serviceType => serviceTypes.push(JSON.parse(serviceType)));
+        debugger
+    })
     
+    return serviceTypes;
 }
 
 async function getManifestationTypes() {
-    var types;
-
-    await $.post(`${getPhpPath()}/manifestation.php`, { function: 'getManfestationTypes' }, (response) => {
-        types = response;
+    var  manifestationTypes = [];
+    
+    // gets all manifestation types from table `tipo_ouvidoria` and adds them all as Objects to manifestationTypes array
+    await $.post(`${getPhpPath()}/manifestation.php`, { function: 'getManifestationTypes' }, (response) => {
+        const responseArr = response.split('//\\').filter(manifestationType => manifestationType.trim());
+        
+        responseArr.forEach(manifestationType => manifestationTypes.push(JSON.parse(manifestationType)));
     })
 
-    return response;
+    return manifestationTypes;
 }
