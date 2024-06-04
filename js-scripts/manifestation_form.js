d773588f8e55e14ccca0bfd310e8b85e46895e38
Bruno <brunombonifacio@gmail.com>
2024-06-03 23:56:34 -0300

    Started working on maanifestation creation

--- js-scripts/manifestation_form.js
+++ js-scripts/manifestation_form.js
@@ -1,13 +1,16 @@
 $('#create_manifestation_btn').on('click', () => {
     const form = document.querySelector('form.needs-validation');
     
-    
     if (!checkEmptyFields(form)) return;
+    
+    // TODO: Insert data on `ouvidoria` table and get its ID, then insert attachments in `anexo` table linked to the manifestation's ID
 
     const fileInput = form.querySelector('#manifestation-attachments');
-    const files = fileInput.files;
 
-    const extensionPattern = /\.(jpe?g|pdf)$/i
+    // assures the files are inside an array, even if its a single file
+    const files = [ ...fileInput.files ];
+
+    const extensionPattern = /\.(jpe?g|pdf|png)$/i
     
     files.forEach(file => {
         // checks if file is type PDF or JPEG
@@ -18,14 +21,19 @@ $('#create_manifestation_btn').on('click', () => {
             
             return;
         }
-
+        
+        // resets error message in case file is valid
         $('#invalid-attachment').text('É necessário pelo menos um anexo.')
+        fileInput.classList.remove('is-invalid');
+
         // convert attached files to B64 and insert in database
         const reader = new FileReader()
         
         reader.readAsDataURL(file)
         reader.onload = () => {
-            
+            const base64Image = reader.result;
+
+            // TODO: insert attachments linked to the manifestation's ID
         }
     })
 })
