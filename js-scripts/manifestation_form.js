a33037c1da0a4cc2a1c1ad81403a1cb8ee38e2e6
Bruno <brunombonifacio@gmail.com>
2024-06-03 23:42:44 -0300

    Started working on file attachment

--- js-scripts/manifestation_form.js
+++ js-scripts/manifestation_form.js
@@ -1,5 +1,31 @@
 $('#create_manifestation_btn').on('click', () => {
-    if (!checkEmptyFields()) {
+    const form = document.querySelector('form.needs-validation');
+    
+    
+    if (!checkEmptyFields(form)) return;
 
-    }
+    const fileInput = form.querySelector('#manifestation-attachments');
+    const files = fileInput.files;
+
+    const extensionPattern = /\.(jpe?g|pdf)$/i
+    
+    files.forEach(file => {
+        // checks if file is type PDF or JPEG
+        if (!extensionPattern.test(file.name)) {
+            $('#invalid-attachment').text('Tipo de arquivo inválido.')
+            fileInput.classList.add('is-invalid');
+            fileInput.classList.remove('is-valid');
+            
+            return;
+        }
+
+        $('#invalid-attachment').text('É necessário pelo menos um anexo.')
+        // convert attached files to B64 and insert in database
+        const reader = new FileReader()
+        
+        reader.readAsDataURL(file)
+        reader.onload = () => {
+            
+        }
+    })
 })
