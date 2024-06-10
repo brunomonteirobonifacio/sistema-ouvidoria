ed36d449ccfb5bc69bbd9ceae7c4b9960d88f54e
Bruno <brunombonifacio@gmail.com>
2024-06-10 11:42:13 -0300

    Fixed Navbar spelling mistake

--- pages/create_manifestation.php
+++ pages/create_manifestation.php
@@ -56,7 +56,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="../">Ouvidoria Municipal</a>
+            <a class="navbar-brand" href="../">Ouvidoria Pública</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
