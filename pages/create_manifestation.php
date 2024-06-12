38e9c47d5001e80556be0833d187ee62d7bc1440
Bruno <brunombonifacio@gmail.com>
2024-06-12 09:34:31 -0300

    Altered page titles

--- pages/create_manifestation.php
+++ pages/create_manifestation.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <title>Abrir Ouvidoria</title>
+    <title>Abrir Ouvidoria - Ouvioria Pública</title>
 </head>
 <body>
     <?php
--- pages/signup.php
+++ pages/signup.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <title>Crie uma conta</title>
+    <title>Crie uma conta - Ouvidoria Pública</title>
 </head>
 <body>
     <?php
